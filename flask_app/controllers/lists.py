c1213ea9708f8ea41f6185ee13d8fbd1ed48bd1f
Ron Murphy <ronmurphy33@gmail.com>
2022-04-18 14:53:15 -0400

    created list class and routes

--- flask_app/controllers/lists.py
+++ flask_app/controllers/lists.py
@@ -3,6 +3,7 @@ from turtle import clear
 from flask_app import app 
 from flask import render_template, redirect, flash, request, session 
 from flask_app.models import user, list
+from flask_app.models.list import List
 from flask_app.models.user import User
 
 @app.route ('/new_list')
@@ -11,6 +12,13 @@ def show_new_list():
         return redirect ('/')
     return render_template("new_list.html")
 
-@app.route('/create_list', methods=['POST']
+@app.route ('/create_list', methods=['POST'])
 def create_list():
-    pass
+    if 'user_id' not in session:
+        return redirect ('/')
+    data = {
+        "list_name" : request.form['list_name'],
+        "user_id": session['user_id']
+    }
+    List.create_list(data)
+    return redirect('/dashboard')
