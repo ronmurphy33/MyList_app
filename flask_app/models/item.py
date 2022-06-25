d3bdda899a2ab61e15bcd6518cf9d552dbec0583
Ron Murphy <ronmurphy33@gmail.com>
2022-06-25 12:24:47 -0400

    changes

--- flask_app/models/item.py
+++ flask_app/models/item.py
@@ -19,28 +19,6 @@ class Item:
     def add_item(cls, data):
         query = "INSERT into items (item_name, category, list_id) VALUES (%(item_name)s, %(category)s, %(list_id)s)"
         results = MySQLConnection(cls.schema).query_db(query, data)
-    
-    # @classmethod
-    # def get_all_items_on_list(cls, data):
-    #     query = "SELECT * FROM items LEFT JOIN lists on items.list_id = %(id)s"
-    #     results = connectToMySQL(cls.schema).query_db(query)
-    #     items_all = []
-    #     for this_item in results:
-    #         item_instance = cls(this_item)
-    #         list_data = {
-    #             "id" : this_item['id'],
-    #             "list_name" : this_item['list_name'],
-    #             "created_at" : this_item['created_at'],
-    #             "updated_at" : this_item['updated_at']
-    #         }
-    #         this_list =list.List(list_data)
-    #         item_instance.list = this_list
-    #         items_all.append(item_instance)
-    #     return items_all
-    #     for r in results:
-    #         items_all.append((cls(r)))
-    #     print(items_all)
-    #     return items_all
 
     @classmethod
     def get_all_items(cls, data):
