098f0974eac8936647348cc8d927a7826bb6cade
Ron Murphy <ronmurphy33@gmail.com>
2022-09-11 11:16:45 -0400

    removing unneeded import package

--- flask_app/controllers/items.py
+++ flask_app/controllers/items.py
@@ -1,6 +1,4 @@
 import re
-from turtle import clear
-from typing import ItemsView
 from flask_app import app 
 from flask import render_template, redirect, flash, request, session 
 from flask_app.models import user, list, item
